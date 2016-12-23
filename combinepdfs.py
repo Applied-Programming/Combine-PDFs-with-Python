8e510b6c3f5fde0ddded4d20b276d20df8c88048
Aniruddha-Tapas <aniruddhastapas@gmail.com>
2016-12-23 23:51:06 +0530

    Commit

--- combinepdfs.py
+++ combinepdfs.py
@@ -1,33 +1,41 @@
-import PyPDF2, os
+# Python3.5
+# combinepdfs.py
+# _______________
+
+import PyPDF2
+import os
 
 # Get all the PDF filenames.
-pdfFiles = []
-for filename in os.listdir('.'):
-    if filename.endswith('.pdf'):
-        pdfFiles.append(filename)
-pdfFiles.sort()
+# Store in the pdf_files list.
+pdf_files = []
+for file in os.listdir('.'):
+    if file.endswith('.pdf'):
+        pdf_files.append(file)
+pdf_files.sort()
+print (pdf_files)
+# Sort the PDF files 
 
-pdfWriter = PyPDF2.PdfFileWriter()
+# Create an object of the PdfFileWriter()class from the PyPDF2 module
+pdf_writer = PyPDF2.PdfFileWriter()
 
-# Loop through all the PDF files.
-for filename in pdfFiles:
-    pdfFileObj = open(filename, 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+# Iterate through all the PDFs.
+for file in pdf_files:
+    pdf_obj = open(file, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_obj)
 
-    # Loop through all the pages and add them.
+    # Loop through all pages of the PDFs and append them.
     
-    # If you want to skip the first page, you should use- 
+    # If you wish to skip the first page of the PDF, you can use:
     #for pageNum in range(1, pdfReader.numPages):
     
-    # If you want to skip the first 2 pages, you should use-
-    #for pageNum in range(2, pdfReader.numPages):
-    #and so on..
+    # In general, to skip the first 'n' pages, use:
+    #for pageNum in range(n, pdfReader.numPages):
     
-    for pageNum in range(0, pdfReader.numPages):
-        pageObj = pdfReader.getPage(pageNum)
-        pdfWriter.addPage(pageObj)
+    for page_no in range(0, pdf_reader.numPages): #no skipping (0)
+        page = pdf_reader.getPage(page_no)
+        pdf_writer.addPage(page)
 
 # Save the resulting PDF to a file.
-pdfOutput = open('combined.pdf', 'wb')
-pdfWriter.write(pdfOutput)
-pdfOutput.close()
+pdf_output = open('CombinedPDFs.pdf', 'wb')
+pdf_writer.write(pdf_output)
+pdf_output.close()
